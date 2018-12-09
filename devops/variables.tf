5f5f3ebd7cc58ce4bd312e61562d0b3f57bc4cf9
Ke_Giang <yusuke.social.ebisu@gmail.com>
2018-12-09 12:50:39 +0100

    Added devops codes to set up vpc on ec2

---
+++ devops/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_profile" {
+  default = "terraform"
+}
+
+variable "aws_region" {
+  default = "eu-central-1"
+}
+
+variable "app_name" {
+  default = "laravel-k83"
+}
+
+variable "env" {
+  default = "dev"
+}
